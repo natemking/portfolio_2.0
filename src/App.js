c24403af5378fd9154482fbb338ca17eae8ddefd
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-05 16:05:38 -0500

    Navbar built, router and boostrap added, bg image set

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { HashRouter as Router, Route } from 'react-router-dom';
+import bgImage from './nate-in-smoke.jpg';
+import './App.css'
 
 function App() {
   return (
-    <div >
-      
-    </div>
+    <>
+      <img id="bg" src={ bgImage } alt="nate in smoke"></img>
+      <Router>
+        <Navbar />
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light">
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link to="/" className={ window.location.pathname === "/" || window.location.pathname === "/about" ? "nav-link active" : "nav-link" }>
+                            About
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/discover" className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}>
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link to="/search" className={window.location.pathname === "/search" ? "nav-link active" : "nav-link"}>
+                            Resume
+                        </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
