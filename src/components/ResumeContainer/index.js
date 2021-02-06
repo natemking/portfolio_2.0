4e068a65ae59091cea0e3c3b519e7a97c6734394
Nate M King <72567521+natemking@users.noreply.github.com>
2021-02-06 10:19:05 -0500

    Created page number toggle on Resume

--- src/components/ResumeContainer/index.js
+++ src/components/ResumeContainer/index.js
@@ -4,12 +4,20 @@ import res1 from './res-1.jpg';
 import res2 from './res-2.jpg'
 
 const ResumeContainer = () => {
-    const [page, setPage] = useState(res1)
+    const [page, setPage] = useState(res1);
+    const [pageNumber, setPageNumber] = useState('pg 1');
 
+    const nextPage = () => {
+      page === res1 ? setPage(res2) : setPage(res1);
+    }
+
+    const togglePageNumber = () => {
+      page !== res1 ? setPageNumber('pg 1') : setPageNumber('pg 2');
+    }
 
     return ( 
         <section className="container">
-            
+
             <section className="container__title ">
                <section> 
                    <h3>
@@ -17,7 +25,9 @@ const ResumeContainer = () => {
                     </h3>
                 </section>
                 <section>
-                    <i className="fas fa-arrows-alt-h"></i>
+                    <span className={pageNumber === 'pg 1' ? 'show' : 'hidden'}>{ pageNumber }</span>
+                    <i className="fas fa-arrows-alt-h" onClick={() => { nextPage(); togglePageNumber(); }}></i>
+                    <span className={pageNumber === 'pg 2' ? 'show' : 'hidden'}>{ pageNumber }</span>
                 </section>
                 <section>
                     <a href="/misc/NateMKing-resume.pdf" download>
